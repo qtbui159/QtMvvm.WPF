fecb8850e299a98245be9d2fd16d426f4ebdb362
qidong5566 <635877990@qq.com>
2019-03-28 17:29:01 +0800

    增加dialog的常用方法

---
+++ src/Sample/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using QtMvvm.WPF.App;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Example.Views;
+using QtMvvm.WPF.Module;
+using ModuleA;
+
+namespace Example
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : QtApplication
+    {
+        protected override Window CreateMainWindow()
+        {
+            return new MainWindow();
+        }
+
+        protected override void RegisterModule()
+        {
+            
+        }
+
+        protected override bool PrepareCreateMainWindow()
+        {
+            ModuleManager.Add<ModuleAModule>();
+
+            return true;
+        }
+
+    }
+}
